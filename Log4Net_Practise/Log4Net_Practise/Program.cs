f716d958738a66f1443672dad281e63005dd869e
Jagadeesh Junjur <jagdish.junjur@gmail.com>
2022-11-14 18:49:40 +0530

    log4net - RollingFileAppender

--- Log4Net_Practise/Log4Net_Practise/Program.cs
+++ Log4Net_Practise/Log4Net_Practise/Program.cs
@@ -6,7 +6,15 @@ namespace Log4Net_Practise
     {
         static void Main(string[] args)
         {
+            log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            log.Info("INFO Log");
+            log.Debug("Debug Log");
+            log.Warn("Warn Log");
+            log.Error("Error Log");
+
             Console.WriteLine("Hello World!");
+
         }
     }
 }
