e1a2dd5e8f9015f3f4aca1690a09766f24ee383a
Jagadeesh Junjur <jagdish.junjur@gmail.com>
2022-11-14 18:16:45 +0530

    Console App

---
+++ Log4Net_Practise/Log4Net_Practise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Log4Net_Practise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
